37dd8aa398f28af2a52ff5d57b359c839c40c136
George <z51683567@livemail.tw>
2023-01-11 05:57:04 +0800

    Initial Commit

---
+++ Perfect Education with CYCU/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Perfect Education with CYCU
+//
+//  Created by 范喬智 on 2023/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Perfect Education with CYCU/Perfect_Education_with_CYCUApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perfect_Education_with_CYCUApp.swift
+//  Perfect Education with CYCU
+//
+//  Created by 范喬智 on 2023/1/11.
+//
+
+import SwiftUI
+
+@main
+struct Perfect_Education_with_CYCUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
